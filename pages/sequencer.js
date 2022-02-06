2314e071d95de62c93710b63d26a96f81e8a1d2a
Eric Dulcet <etdulcet@gmail.com>
2022-02-06 12:44:31 -0500

    Clean up sequencer code, need to fix setting notes to play effect

--- pages/sequencer.js
+++ pages/sequencer.js
@@ -5,69 +5,10 @@ import React, { useContext, useEffect, useState } from "react";
 import Head from "next/head";
 
 const Steps = ({ row, rowIndex }) => {
-  const rowex = [
-    {
-      note: "C4",
-      step: 0,
-      selected: false,
-      id: "C4-0",
-    },
-    {
-      note: "C4",
-      step: 1,
-      selected: false,
-      id: "C4-1",
-    },
-    {
-      note: "C4",
-      step: 2,
-      selected: false,
-      id: "C4-2",
-    },
-    {
-      note: "C4",
-      step: 3,
-      selected: false,
-      id: "C4-3",
-    },
-    {
-      note: "C4",
-      step: 4,
-      selected: false,
-      id: "C4-4",
-    },
-    {
-      note: "C4",
-      step: 5,
-      selected: false,
-      id: "C4-5",
-    },
-    {
-      note: "C4",
-      step: 6,
-      selected: false,
-      id: "C4-6",
-    },
-    {
-      note: "C4",
-      step: 7,
-      selected: false,
-      id: "C4-7",
-    },
-  ];
-
+  const [info, setters] = useContext(SeqContext);
   return (
     <span className="step-container">
       {row.map((currentStep) => {
-        const [selected, setSelected] = useState(false);
-        const [info, setters] = useContext(SeqContext);
-        const synth = info.synth;
-        useEffect(() => {
-          if (info.step === currentStep.step && currentStep.selected) {
-            // synth.triggerAttackRelease(note, "4n", info.time);
-            setters.setNotes([...info.notes, currentStep.note]);
-          }
-        }, [currentStep.step, currentStep.selected]);
         return (
           <div
             className={`step ${
@@ -108,45 +49,33 @@ const Steps = ({ row, rowIndex }) => {
         .current.selected {
           transform: scale(1.25);
         }
-        @media (max-width: 600px) {
-        }
       `}</style>
     </span>
   );
 };
 
-const Notes = () => {
-  // const notesarr = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"];
-  // const stepsnum = [0, 1, 2, 3, 4, 5, 6, 7];
-
-  // const matrix = notesarr.map((note) => {
-  //   return stepsnum.map((step) => {
-  //     return { note, step, selected: false, id: `${note}-${step}` };
-  //   });
-  // });
-  const [info, setters] = useContext(SeqContext);
+const Rows = () => {
+  const [info] = useContext(SeqContext);
 
   return (
-    <div className="note-container">
+    <div className="row-container">
       {info.matrix.map((row, rowIndex) => {
         return (
-          <span className="note-row" key={`note-row-${rowIndex}`}>
+          <span className="row" key={`row-${rowIndex}`}>
             <Steps row={row} rowIndex={rowIndex} />
           </span>
         );
       })}
       <style jsx>{`
-        .note-row {
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-        }
-        .note-container {
+        .row-container {
           display: flex;
           flex-direction: column;
           justify-content: space-between;
         }
-        @media (max-width: 600px) {
+        .row {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
         }
       `}</style>
     </div>
@@ -197,6 +126,7 @@ const SeqProvider = ({ children }) => {
   // set notes to play
   useEffect(() => {
     if (step >= 0) {
+      console.log("setting notes");
       const notess = matrix.map((row) => {
         if (row[step].selected) {
           return row[step].note;
@@ -214,7 +144,7 @@ const SeqProvider = ({ children }) => {
       synth.triggerAttackRelease(notes, "4n", time);
       setNotes([]);
     }
-  }, [playNotes, notes]);
+  }, [playNotes, notes, synth, time]);
 
   const info = { step, synth, time, notes, matrix };
   const setters = { setStep, setNotes, updateNote };
@@ -225,22 +155,9 @@ const SeqProvider = ({ children }) => {
 };
 
 const Sequencer = () => {
-  const [info, setStep] = useContext(SeqContext);
-  Tone.Transport.bpm.value = 120;
-
   const [started, setStarted] = useState(false);
-  // const [time, setTime] = useState();
-  // const [step, setStep] = useState(0);
 
-  useEffect(() => {
-    // let x = 0;
-    // Tone.Transport.scheduleRepeat((time) => {
-    //   // use the callback time to schedule events
-    //   setStep(x);
-    //   setTime(time);
-    //   x = (x + 1) % 8;
-    // }, "8n");
-  }, []);
+  Tone.Transport.bpm.value = 120;
 
   return (
     <div>
@@ -260,15 +177,13 @@ const Sequencer = () => {
       >
         {started ? "Started" : "Stopped"}
       </h1>
-      <Notes step={info.step} />
+      <Rows />
       <style jsx>{`
         .title {
           margin: 0;
           line-height: 1.15;
           font-size: var(--text-extra-large);
           color: var(--light);
-        }
-        @media (max-width: 600px) {
         }
       `}</style>
     </div>
@@ -277,41 +192,29 @@ const Sequencer = () => {
 
 const Wrappa = () => {
   const windowLoaded = typeof window !== "undefined";
-  if (windowLoaded) {
-    return (
-      <div className="container">
-        <Head>
-          <title>Sampler</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main>
+  return (
+    <div>
+      <Head>
+        <title>Sampler</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        {windowLoaded ? (
           <SeqProvider>
             <Sequencer />
           </SeqProvider>
-        </main>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <Head>
-          <title>Sampler</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main>
-          <div className="loading">LOADING</div>;
-        </main>
-        <style jsx>{`
-          .loading {
-            margin: 0;
-            line-height: 1.15;
-            color: var(--light);
-          }
-          @media (max-width: 600px) {
-          }
-        `}</style>
-      </div>
-    );
-  }
+        ) : (
+          <div className="loading">LOADING</div>
+        )}
+      </main>
+      <style jsx>{`
+        .loading {
+          margin: 0;
+          line-height: 1.15;
+          color: var(--light);
+        }
+      `}</style>
+    </div>
+  );
 };
 export default Wrappa;
